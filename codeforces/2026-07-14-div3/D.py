42c08f2eca0b1ce442afa5c4e606af3902c0823f
omkarshirwadkar <omkarshirwadkar1693@gmail.com>
2026-07-30 23:39:08 +0530

    Greedy Solution for removing digits

---
+++ codeforces/2026-07-14-div3/D.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n, m = [int(s) for s in input().split()]
+    a = [int(s) for s in input().split()]
+    b = [int(s) for s in input().split()]
+    b.sort()
+    
