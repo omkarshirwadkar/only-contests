9bd21a8ee178a05257c69810db75421e08a1acb5
omkarshirwadkar <omkarshirwadkar1693@gmail.com>
2026-08-06 23:23:50 +0530

    C optimized

--- codeforces/2026-08-06-div2/C.py
+++ codeforces/2026-08-06-div2/C.py
@@ -1,32 +1,45 @@
 import heapq
+
 t = int(input())
 for _ in range(t):
     n, m = [int(s) for s in input().split()]
     v = [int(s) for s in input().split()]
     a = []
-    minArray = []
-    ans = m
     for i in range(n):
         r = [int(s) for s in input().split()]
-        minArray.append(min(r))
         a.append(r)
-    temp = 0
-    t1 = sorted(a[-1], reverse=True)
-    for k in range(ans):
-        v[-1] -= a[-1][k]
-        if v[-1] <= 0:
+        
+    last_row = sorted(a[-1], reverse=True)
+    ans = m
+    current_sum = 0
+    for k in range(m):
+        current_sum += last_row[k]
+        if current_sum >= v[-1]:
             ans = k + 1
             break
-    numbers = a[-1]
-    heapq.heapify(numbers)
+            
+    min_heap = last_row[:ans]
+    heapq.heapify(min_heap)
+    heap_sum = sum(min_heap)
+    
     for i in range(n - 2, -1, -1):
-        for j in range(m):
-            heapq.heappush(numbers, a[i][j])
-        largest_ans = heapq.nlargest(ans, numbers)
-        temp = 0
+        row = a[i]
+        for val in row:
+            if val > min_heap[0]:
+                heap_sum += val - heapq.heapreplace(min_heap, val)
+        
+        if heap_sum < v[i]:
+            continue
+            
+        sorted_largest = sorted(min_heap, reverse=True)
+        temp_sum = 0
         for k in range(ans):
-            v[i] -= largest_ans[k]
-            if v[i] <= 0:
+            temp_sum += sorted_largest[k]
+            if temp_sum >= v[i]:
                 ans = k + 1
                 break
+        
+        while len(min_heap) > ans:
+            heap_sum -= heapq.heappop(min_heap)
+            
     print(ans)
